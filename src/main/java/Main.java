98341320d902c7ebe5a8a8ec235df2022fb04f6c
dangeonMaster <dafae722@gmail.com>
2023-05-03 17:41:11 +0300

    complete

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        UIclass ui=new UIclass();
+        ui.init();
+    }
+}
---
+++ src/main/java/UIclass.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+
+
+public class UIclass extends Thread {
+
+    private JFrame uiF;
+    private JPanel uiP;
+    private DataBase sql;
+    private String user;
+
+    UIclass()
+    {
+        uiF = new JFrame();
+        sql=new DataBase();
+    }
+
+    public void init()
+    {
+        uiF.setTitle("Airport Managment System");
+        uiF.setSize(1000,100);
+        uiF.setExtendedState(uiF.getExtendedState()|JFrame.MAXIMIZED_BOTH);
+        uiF.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        uiF.setVisible(true);
+        this.uiP = new Login(this,sql);
+        uiF.setContentPane(uiP);
+        uiF.revalidate();
+    }
+
+    public void setupSystem(){
+        this.uiP=new AMSsystem(sql,this,user);
+        uiF.setContentPane(uiP);
+        uiF.revalidate();
+    }
+
+    public void logout()
+    {
+        this.uiP=new Login(this,sql);
+        uiF.setContentPane(uiP);
+        uiF.revalidate();
+    }
+
+    public void setUser(String user)
+    {
+        this.user=user;
+    }
+}
